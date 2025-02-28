26edb84280f7acdfbb70ffc475dc66d1a388d74d
Winston Bravo <wbravoanoni@gmail.com>
2025-02-27 21:12:07 -0300

    prueba server render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+    host: "0.0.0.0", // Permite accesos desde cualquier IP
+  },
+  build: {
+    outDir: "dist",
+  },
+  preview: {
+    port: 8080,
+    host: "0.0.0.0",
+    allowedHosts: ["proyecto-final-desafiolatam.onrender.com"] // Agrega tu dominio aquí
+  }
+});
