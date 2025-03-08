c9ce65bf8615646565a9707e92354cd145926cd2
Winston Bravo <wbravoanoni@gmail.com>
2025-03-08 13:18:54 -0300

    correccion de rutas

--- src/pages/usuarios/FormularioComprador.jsx
+++ src/pages/usuarios/FormularioComprador.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer";
-import NavbarUser from '../components/NavbarUser';
+import NavbarUser from '../../components/NavbarUser';
 
 const FormularioComprador = () => {
   const navigate = useNavigate();
