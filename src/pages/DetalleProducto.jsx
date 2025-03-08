6d7f9cf0665b110a6e7e090d0ce2347488e29bd5
Winston Bravo <wbravoanoni@gmail.com>
2025-03-08 14:04:39 -0300

    eliminar navbar repetido

--- src/pages/DetalleProducto.jsx
+++ src/pages/DetalleProducto.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Navbar from '../components/Navbar';
 import NavbarAdmin from '../components/NavbarAdmin';
-import NavbarAdmin from '../components/NavbarAdmin';
 import NavbarUser from '../components/NavbarUser';
 import Footer from '../components/Footer';
 import ProductoUnico from '../components/ProductoUnico';
