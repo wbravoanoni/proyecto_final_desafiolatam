f6006941b4adf6d1ff045194198a521f2b461187
Winston Bravo <wbravoanoni@gmail.com>
2025-03-08 22:33:39 -0300

    actualziacion componente producto nuevo

--- src/components/ProductosNuevos.jsx
+++ src/components/ProductosNuevos.jsx
@@ -1,37 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React, { useContext,useState,useEffect } from "react";
 
 const ProductosNuevos = () => {
-    const slides = [
-      {
-        cards: [
-          { title: 'Producto 1', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/392017-300-400?v=638603638076030000&width=300&height=400&aspect=true' },
-          { title: 'Producto 2', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/392212-300-400?v=638603640519630000&width=300&height=400&aspect=true' },
-          { title: 'Producto 3', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/397599-300-400?v=638603704103400000&width=300&height=400&aspect=true' },
-        ],
-      },
-      {
-        cards: [
-          { title: 'Producto 4', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/355750-300-400?v=638452777358900000&width=300&height=400&aspect=true' },
-          { title: 'Producto 5', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/401618-300-400?v=638696284751200000&width=300&height=400&aspect=true' },
-          { title: 'Producto 6', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/405174-300-400?v=638736488202370000&width=300&height=400&aspect=true' },
-        ],
-      },
-      {
-        cards: [
-          { title: 'Producto 7', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/368856-300-400?v=638537208254370000&width=300&height=400&aspect=true' },
-          { title: 'Producto 8', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/368528-300-400?v=638512140669130000&width=300&height=400&aspect=true' },
-          { title: 'Producto 9', price: '$9', text: 'Nombre Producto', image: 'https://columbiacl.vtexassets.com/arquivos/ids/355200-300-400?v=638452762264370000&width=300&height=400&aspect=true' },
-        ],
-      },
-    ];
+
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+          try {
+              const response = await fetch(`https://api-fake-sport.onrender.com/api/productos`);
+              if (!response.ok) throw new Error("Error al obtener los datos");
+              const data = await response.json();
+              setProductos(data);
+          } catch (error) {
+              console.error("Error:", error);
+          } finally {
+              setLoading(false);
+          }
+      };
+  
+      fetchData();
+  }, []);
+
   
     return (
         <div className="row">
             <h3 className="text-center py-3">Productos Nuevos</h3>
             <div id="carouselWithCards" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-                {slides.map((slide, index) => (
+                  {productos.map((producto) => (
                     <div
                     className={`carousel-item ${index === 0 ? 'active' : ''}`}
                     key={index}
@@ -41,10 +39,9 @@ const ProductosNuevos = () => {
                         {slide.cards.map((card, idx) => (
                             <div className="col-md-4" key={idx}>
                             <div className="card">
-                                <img src={card.image} className="card-img-top" alt={card.title}/>
+                                <a href={`/productos/${producto.id}`}><img src={producto.imagen} className="card-img-top" alt={producto.nombre}/></a>
                                 <div className="card-body text-center">
-                                <h5 className="card-title">{card.price}</h5>
-                                <p className="card-text">{card.text}</p>
+                                <h5 className="card-title">{producto.precio}</h5>
                                 </div>
                             </div>
                             </div>
